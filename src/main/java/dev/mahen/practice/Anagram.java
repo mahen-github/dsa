8ddc12c118d8745e74ab25e5cab967c5964d3a92
mahendran ponnusamy <mahendran.ponnusamy@nordstrom.com>
2019-11-03 21:55:29 -0800

    Anagram

--- src/main/java/dev/mahen/practice/Anagram.java
+++ src/main/java/dev/mahen/practice/Anagram.java
@@ -30,5 +30,12 @@ public class Anagram {
     return new ArrayList(map.values());
   }
 
+  /**
+   * Complexity Analysis
+   *
+   * Time Complexity: O(NK \log K)O(NKlogK), where NN is the length of strs, and KK is the maximum length of a string in strs. The outer loop has complexity O(N)O(N) as we iterate through each string. Then, we sort each string in O(K \log K)O(KlogK) time.
+   *
+   * Space Complexity: O(NK)O(NK), the total information content stored in ans.
+   */
 
 }
